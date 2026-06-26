e729b826d6dadbd37184d4f577f9325dc4d07495
Patricio Carvalho <patr.solothurn@gmail.com>
2026-06-26 10:30:13 +0200

    Add escolar_perfil_2_dados.sql

---
+++ supabase-functions/escolar_perfil_2_dados.sql
@@ -0,0 +1,2 @@
+insert into escolar_perfil (aluno, klasse, cidade, resp_nome, resp_sala, resp_tel, resp_email)
+values ('lucas', 'SEK P', 'Grenchen', 'Petra Humair', '3-01', '079 763 71 86', 'petra.humair@schulen-grenchen.ch');
