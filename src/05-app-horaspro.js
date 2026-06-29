f42348472648d5f17cd3418a82c6df02b6586b3e
Patricio Carvalho <patr.solothurn@gmail.com>
2026-06-30 00:35:20 +0200

    Patricio Time: calendario do popup Calendario e Stats passa a navegar entre meses (botoes anterior/seguinte), em vez de fixo em Junho 2026

--- src/05-app-horaspro.js
+++ src/05-app-horaspro.js
@@ -378,6 +378,25 @@ function HorasProApp(_ref9) {
     _useState0 = _slicedToArray(_useState9, 2),
     menuMonth = _useState0[0],
     setMenuMonth = _useState0[1];
+  var MESES_CAL = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  var calYear = menuMonth.getFullYear();
+  var calMonthIdx = menuMonth.getMonth();
+  var calMonthLabel = MESES_CAL[calMonthIdx] + ' ' + calYear;
+  var calDaysInMonth = new Date(calYear, calMonthIdx + 1, 0).getDate();
+  var calFirstDow = new Date(calYear, calMonthIdx, 1).getDay();
+  var calFirstDowMon = (calFirstDow + 6) % 7;
+  // Constrói as semanas (Seg-Dom) do mês escolhido, com células vazias
+  // (null) antes do dia 1 e depois do último dia, para alinhar a grelha.
+  var calWeeks = [];
+  var calDayCounter = 1 - calFirstDowMon;
+  while (calDayCounter <= calDaysInMonth) {
+    var calWeekDays = [];
+    for (var calWdi = 0; calWdi < 7; calWdi++) {
+      calWeekDays.push(calDayCounter >= 1 && calDayCounter <= calDaysInMonth ? calDayCounter : null);
+      calDayCounter++;
+    }
+    calWeeks.push(calWeekDays);
+  }
   var _useState1 = (0, _react.useState)(false),
     _useState10 = _slicedToArray(_useState1, 2),
     showAdd = _useState10[0],
@@ -4428,15 +4447,54 @@ function HorasProApp(_ref9) {
       letterSpacing: '1.5px',
       textTransform: 'uppercase'
     }
-  }, "Calend\xE1rio & Stats")), /*#__PURE__*/React.createElement("p", {
+  }, "Calend\xE1rio & Stats")), /*#__PURE__*/React.createElement("div", {
+    style: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: 14
+    }
+  }, /*#__PURE__*/React.createElement("button", {
+    onClick: function onClick() {
+      return setMenuMonth(function (m) {
+        return new Date(m.getFullYear(), m.getMonth() - 1, 1);
+      });
+    },
+    style: {
+      width: 32,
+      height: 32,
+      borderRadius: '50%',
+      border: "1px solid ".concat(H.border),
+      background: H.surface2,
+      color: H.gold,
+      fontSize: 16,
+      fontWeight: 700,
+      cursor: 'pointer'
+    }
+  }, "\u2039"), /*#__PURE__*/React.createElement("p", {
     style: {
       color: H.gold,
       fontWeight: 800,
-      fontSize: 16,
-      textAlign: 'center',
-      marginBottom: 14
+      fontSize: 16
     }
-  }, "Junho 2026"), /*#__PURE__*/React.createElement("div", {
+  }, calMonthLabel), /*#__PURE__*/React.createElement("button", {
+    onClick: function onClick() {
+      return setMenuMonth(function (m) {
+        return new Date(m.getFullYear(), m.getMonth() + 1, 1);
+      });
+    },
+    style: {
+      width: 32,
+      height: 32,
+      borderRadius: '50%',
+      border: "1px solid ".concat(H.border),
+      background: H.surface2,
+      color: H.gold,
+      fontSize: 16,
+      fontWeight: 700,
+      cursor: 'pointer'
+    }
+  }, "\u203A")), /*#__PURE__*/React.createElement("div", {
     style: {
       marginBottom: 16
     }
@@ -4457,12 +4515,13 @@ function HorasProApp(_ref9) {
         color: H.gold
       }
     }, d);
-  })), [[23, [1, 2, 3, 4, 5, 6, 7]], [24, [8, 9, 10, 11, 12, 13, 14]], [25, [15, 16, 17, 18, 19, 20, 21]], [26, [22, 23, 24, 25, 26, 27, 28]], [27, [29, 30]]].map(function (_ref11) {
-    var _ref12 = _slicedToArray(_ref11, 2),
-      kw2 = _ref12[0],
-      ds = _ref12[1];
+  })), calWeeks.map(function (week, wi) {
+    var firstRealDay = week.find(function (d) {
+      return d !== null;
+    });
+    var weekKw = getKW(new Date(calYear, calMonthIdx, firstRealDay, 12));
     return /*#__PURE__*/React.createElement("div", {
-      key: kw2,
+      key: wi,
       style: {
         display: 'grid',
         gridTemplateColumns: '36px repeat(7,1fr)',
@@ -4481,8 +4540,11 @@ function HorasProApp(_ref9) {
         color: H.muted,
         fontWeight: 600
       }
-    }, kw2)), ds.map(function (d) {
-      var dStr = "2026-06-".concat(String(d).padStart(2, '0'));
+    }, weekKw)), week.map(function (d, di) {
+      if (d === null) return /*#__PURE__*/React.createElement("div", {
+        key: 'e' + di
+      });
+      var dStr = "".concat(calYear, "-").concat(String(calMonthIdx + 1).padStart(2, '0'), "-").concat(String(d).padStart(2, '0'));
       var hasEntry = entries.some(function (e) {
         return e.date === dStr;
       });
@@ -4526,12 +4588,6 @@ function HorasProApp(_ref9) {
           margin: '2px auto 0'
         }
       }));
-    }), Array.from({
-      length: 7 - ds.length
-    }).map(function (_, i) {
-      return /*#__PURE__*/React.createElement("div", {
-        key: 'e' + i
-      });
     }));
   })), /*#__PURE__*/React.createElement("p", {
     style: {
