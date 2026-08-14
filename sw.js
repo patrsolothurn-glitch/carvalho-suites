f2a7e18b2c24863fecf600f6b3be7935b854de73
Patricio Carvalho <patr.solothurn@gmail.com>
2026-08-14 13:26:43 +0200

    Deploy automático (sw bump)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Carvalho Suite Service Worker ──────────────────────────────────
-const CACHE = 'carvalho-v4c3b4baf';
+const CACHE = 'carvalho-vfc744cbf';
 const ASSETS = [
   './',
   './index.html',
