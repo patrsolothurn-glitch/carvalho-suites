8cd36d975d793599834b8ab33ed8c318144cf735
Patricio Carvalho <patr.solothurn@gmail.com>
2026-08-22 13:45:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Carvalho Suite Service Worker ──────────────────────────────────
-const CACHE = 'carvalho-v188c98e3';
+const CACHE = 'carvalho-v497968db';
 const ASSETS = [
   './',
   './index.html',
