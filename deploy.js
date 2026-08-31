81162d76d02f3a0179f3090470cf855da262c2b5
Patricio Carvalho <patr.solothurn@gmail.com>
2026-08-31 22:23:03 +0200

    Hoje: remover duplicados escolar (filtrar source=escolar de family_events)

--- deploy.js
+++ deploy.js
@@ -90,8 +90,7 @@ const SRC_FILES = [
   'src/head.html', 'src/01-helpers.js', 'src/02-theme.js', 'src/03-data.js',
   'src/04-ui-components.js', 'src/05-app-horaspro.js', 'src/06-app-agendapro.js',
   'src/07-app-familia.js', 'src/08-app-nutriguima.js', 'src/09-app-escolar.js', 'src/12-app-lucas.js',
-  'src/11-app-subby.js', 'src/13-app-hauswart.js', 'src/14-app-rapport.js', 'src/15-app-viagens.js',
-  'src/10-shell.js', 'src/tail.html',
+  'src/11-app-subby.js', 'src/10-shell.js', 'src/tail.html',
   'build.js', 'deploy.js',
 ];
 
